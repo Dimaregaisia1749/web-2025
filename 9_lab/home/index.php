ae50734b67e3534ab7e5fdd4c51c9e1bafbf6f94
Dmitry <dmeln1749@gmail.com>
2025-04-26 12:38:14 +0300

    fix

--- 9_lab/home/index.php
+++ 9_lab/home/index.php
@@ -3,28 +3,43 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Login</title>
 </head>
 
 <body>
-    <div class="navigation">
-        <a href="http://localhost:8001/home/">
-            <img src="src/home_active.png" alt="Home" class="navigation-image">
-        </a>
-        <a href="http://localhost:8001/profile/?id=1">
-            <img src="src/profile.png" alt="Profile" class="navigation-image">
-        </a>
-        <img src="src/plus.png" alt="Plus" class="navigation-image">
+    <!-- Navigation Block -->
+    <div class="nav">
+        <div class="nav__list">
+            <div class="nav__item">
+                <a href="http://localhost:8001/home/" class="nav__link">
+                    <img src="src/home_active.png" alt="Home" class="nav__icon">
+                </a>
+            </div>
+            <div class="nav__item">
+                <a href="http://localhost:8001/profile/?id=1" class="nav__link">
+                    <img src="src/profile.png" alt="Profile" class="nav__icon">
+                </a>
+            </div>
+            <div class="nav__item">
+                <a href="#" class="nav__link">
+                    <img src="src/plus.png" alt="Add" class="nav__icon">
+                </a>
+            </div>
+        </div>
     </div>
+
+    <!-- Posts Block -->
     <div class="posts">
         <?php
         require_once '../data/sql/db_scripts.php';
         $connection = connectDatabase();
         $posts = findAllPosts($connection);
         foreach ($posts as $post) {
+            echo '<div class="posts__item post">';
             include 'post_template.php';
+            echo '</div>';
         }
         ?>
     </div>
--- 9_lab/home/post_template.php
+++ 9_lab/home/post_template.php
@@ -3,32 +3,42 @@ require_once '../data/sql/db_scripts.php';
 $user = findUserInDatabase($connection, $post['user_id']);
 $post_images = findPostImagesInDatabase($connection, $post['id']);
 ?>
-<div class="post">  
-    <div class="userdata">
-        <a href="http://localhost:8001/profile/?id=<?= $user['id'] ?>">
-            <img src=<?= $user['logo_path'] ?> alt="User 1" class="logo">
-            <p class="name"> <?= $user['username'] ?> </p>
+<div class="post">
+    <div class="post__header">
+        <a href="http://localhost:8001/profile/?id=<?= $user['id'] ?>" class="post__user">
+            <img src="<?= $user['logo_path'] ?>" alt="<?= htmlspecialchars($user['username'], ENT_QUOTES) ?>" class="post__user-avatar">
+            <span class="post__user-name"><?= htmlspecialchars($user['username'], ENT_QUOTES) ?></span>
         </a>
-        <img src="src/edit.png" alt="Edit" class="edit">
+        <a href="http://localhost:8001/post/edit/?id=<?= $post['id'] ?>" class="post__edit">
+            <img src="src/edit.png" alt="Edit post" class="post__edit-icon">
+        </a>
     </div>
-    <div class="image-wrapper">
-        <img src=<?= $post_images[0]["image_path"] ?> alt="Content 1" class="post-image">
+
+    <div class="post__carousel">
+        <img src="<?= $post_images[0]['image_path'] ?>" alt="Post image" class="post__image">
         <?php if (count($post_images) > 1): ?>
-            <p class="image-count"> 1/<?= count($post_images)?></p>
-            <img src="src/left.png" alt="Left arrow" class="left">
-            <img src="src/right.png" alt="Right arrow" class="right">
+            <span class="post__counter">1/<?= count($post_images) ?></span>
+            <button type="button" class="post__arrow post__arrow--left">
+                <img src="src/left.png" alt="Previous" class="post__arrow-icon">
+            </button>
+            <button type="button" class="post__arrow post__arrow--right">
+                <img src="src/right.png" alt="Next" class="post__arrow-icon">
+            </button>
         <?php endif; ?>
     </div>
-    <div class="likes">
-        <img src="src/like.png" alt="Like" class="like-image">
-        <p class="like-count"><?= $post['likes'] ?></p>
+
+    <div class="post__actions">
+        <a href="#" class="post__like">
+            <img src="src/like.png" alt="Like" class="post__like-icon">
+            <span class="post__like-count"><?= $post['likes'] ?></span>
+        </a>
     </div>
-    <p class="post-text">
-        <?= $post['content'] ?>
-    </p>
-    <span class="expand">...еще</span>
-    <?php
-    include_once "format_time.php"
-    ?>
-    <p class="time"> <?= timeAgo(strtotime('now') - strtotime($post['created_at'])) ?></p>
-</div> 
+
+    <div class="post__content">
+        <p class="post__text"><?= htmlspecialchars($post['content'], ENT_QUOTES) ?></p>
+        <span class="post__more">...еще</span>
+    </div>
+
+    <?php include_once "format_time.php"; ?>
+    <div class="post__time"><?= timeAgo(strtotime('now') - strtotime($post['created_at'])) ?></div>
+</div>
